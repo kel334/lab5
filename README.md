24735c84ef93a3b4390a6ff1a6cc20c644118ed8
Kendra <kendraerin@Home.localdomain>
2020-10-23 15:38:40 -0700

    this version DOES NOT have C-Z ASCII, needs merging!!! in other news, this has a bit more than before anyways

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # lab5
 For CS126L Lab5
+
+
+Save for later(?) --
+
+
+def temp_ask():
+    print("Hello and welcome to the ASCII station!")
+    direct = input("Please pick a direction, vertical or horizontal: ")
+    word = input("Please enter a word: ")
+
+def temp_bye():
+    print("Your word was {word} and you chose {direct}.")
+    print("Thank you!")
+
+
--- lab5.py
+++ lab5.py
@@ -2,21 +2,41 @@
 # Rebecca Chavez (rlc397)
 
 
+# building blocks for the ASCII char
 art = {'a one' : '***',
        'a two' : '* *',
        'a three' : '*****',
        'a four' : '*   *',
        'a five' : '*   *',
+
        'b one' : '*****',
        'b two' : '**  *',
        'b three' : '****',
        'b four' : '**  *',
        'b five' : '*****'
+
+       # C-Z go here
       }
 
        
 def main():
-    def tests():
+    print("Hello and welcome to the ASCII station!")
+    direct = input("Please pick a direction, vertical or horizontal: ")
+    word = input("Please enter a word: ")
+    W = word.upper()
+    
+
+def prep_word():
+    for i in range(len(W))
+        term = W[i]
+    return term
+
+
+def find_char():
+    # In statement?
+
+
+    # def tests():
        print(' ' + art['a one'] + '   ' + art['b one'])
        print(' ' + art['a two'] + '   ' + art['b two'])
        print(art['a three'] + '  ' + art['b three'])
