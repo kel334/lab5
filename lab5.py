3213ba7504025928672c1732239e12760b27a4a7
Kendra <kendraerin@Home.localdomain>
2020-10-23 14:55:58 -0700

    Fixed the first two letters-ish?

--- lab5.py
+++ lab5.py
@@ -1,27 +1,26 @@
 # Kendra Ludwig (kel334)
 # Rebecca Chavez (rlc397)
 
-art = {'a one' : '****',
-       'a two' : '*  *',
-       'a three' : '****',
-       'a four' : '*  *',
-       'b one' : '****',
-       'b two' : '*  *',
+art = {'a one' : '***',
+       'a two' : '* *',
+       'a three' : '*****',
+       'a four' : '*   *',
+       'a five' : '*   *',
+       'b one' : '*****',
+       'b two' : '**  *',
        'b three' : '****',
-       'b four' : '****'
+       'b four' : '**  *',
+       'b five' : '*****'
       }
 
        
 def main():
     def tests():
-       print(art['a one'])
-       print(art['a two'])
-       print(art['a three'])
-       print(art['a four'])
-       print(art['a one'] + ' ' + art['b one'])
-       print(art['a two'] + ' ' + art['b two'])
-       print(art['a three'] + ' ' + art['b three'])
-       print( art['a four'] + ' ' + art['b four'])
+       print(' ' + art['a one'] + '   ' + art['b one'])
+       print(' ' + art['a two'] + '   ' + art['b two'])
+       print(art['a three'] + '  ' + art['b three'])
+       print(art['a four'] + '  ' + art['b four'])
+       print(art['a five'] + '  ' + art['b five'])
     tests()
 
 
