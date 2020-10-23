84230a39af890e33c5d49e8ede67d3cd9b6ae831
rlchavez3 <72169569+rlchavez3@users.noreply.github.com>
2020-10-23 14:25:46 -0700

    Update lab5.py
    initial test

--- lab5.py
+++ lab5.py
@@ -1,5 +1,16 @@
 # Kendra Ludwig (kel334)
-# Rebecca Chavez ()
+# Rebecca Chavez (rlc397)
 
-
+art = {'a one' : '****', 'a two' : '*  *', 'a three' : '****', 'a four' : '*  *', 'b one' : ''****', 'b two' : '*  *', 'b three' : '****', 'b four' : '****'}
+       
 def main():
+
+def tests():
+       print(art['a one'])
+       print(art['a two'])
+       print(art['a three'])
+       print(art['a four'])
+       print(art['a one] + ' ' + art['b one'])
+       print(art['a two'] + ' ' + art['b two'])
+       print(art['a three'] + ' ' + art['b three'])
+       print( art['a four'] + ' ' + art['b four'])
